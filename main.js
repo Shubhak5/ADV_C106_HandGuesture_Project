50ac90bbeb9707007cc419c73ce0f4489509ea72
Shubhak5 <83910260+Shubhak5@users.noreply.github.com>
2021-09-21 21:27:18 +0530

    ADV_C108_Project
    Added code to predict which hand gesture is captured, and speak it

--- main.js
+++ main.js
@@ -13,6 +13,7 @@ camera = document.getElementById("camera");
 Webcam.attach('#camera');
 
 function take_snapshot() {
+
     Webcam.snap(function(data_uri) {
         document.getElementById("result").innerHTML = "<img id='captured_img' src='" + data_uri + "'/>";
     });
@@ -20,7 +21,7 @@ function take_snapshot() {
 
 console.log(ml5.version); //just to check if ml5 is loaded or not
 
-classifier = ml5.imageClassifier('https: //teachablemachine.withgoogle.com/models/1WHX6zk7K/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/1WHX6zk7K/model.json', modelLoaded);
 
 function modelLoaded() {
     console.log("Model Loaded!");
@@ -29,8 +30,35 @@ function modelLoaded() {
 function speak() {
     var synth = window.speechSynthesis;
 
-    speak_data = "The hand guesture is " + prediction;
+    speak_data = prediction;
     var utterThis = new SpeechSynthesisUtterance(speak_data);
     synth.speak(utterThis);
+}
 
+function check() {
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResult);
+}
+
+function gotResult(error, results) {
+    if (error) {
+        console.log("error is...." + error);
+    } else {
+        console.log(results[0].label + " " + results[1].label + " " + results[2].label);
+        if (results[0].label == "Victory") {
+            document.getElementById("result_emoji_name").innerHTML = "That was a marvelous Victory";
+            prediction = "That was a marvelous Victory";
+            document.getElementById("update_emoji").innerHTML = " &#9996;";
+        } else if (results[0].label == "Thumbs Up") {
+            document.getElementById("result_emoji_name").innerHTML = "All the Best";
+            prediction = "All the Best";
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        } else if (results[0].label == "Excellent") {
+            document.getElementById("result_emoji_name").innerHTML = "This is looking Amazing";
+            prediction = "This is looking Amazing";
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        speak();
+        prediction = "";
+    }
 }
